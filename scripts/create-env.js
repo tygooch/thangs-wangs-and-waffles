5c3c37c7b8838aee22c4c4aa9895e1afb3192f1c
Ty Gooch <gooch.ty@gmail.com>
2019-06-03 06:18:55 -0700

    netlify fixes

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,5 +1,7 @@
 const fs = require("fs")
 fs.writeFileSync(
   "./.npmrc",
-  "@fortawesome:registry=https://npm.fontawesome.com/\n//npm.fontawesome.com/:_authToken=${FONTAWESOME_TOKEN}\n"
+  "@fortawesome:registry=https://npm.fontawesome.com/" +
+    "\n" +
+    "//npm.fontawesome.com/:_authToken=${FONTAWESOME_TOKEN}"
 )
